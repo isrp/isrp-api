0054835db2866d33914aaffc9c6b2997db135fef
Oded Arbel <oded@geek.co.il>
2018-10-21 01:34:47 +0300

    add google sheets api access

---
+++ src/Isrp/Tools/GoogleSheets.php
@@ -0,0 +1,28 @@
+<?php
+namespace Isrp\Tools;
+
+use Isrp\Tools\GoogleSheets\Sheet;
+
+class GoogleSheets {
+
+	var $client;
+	
+	public function __construct() {
+		$this->client = new \Google_Client();
+		$this->client->useApplicationDefaultCredentials();
+		$this->client->addScope(\Google_Service_Sheets::SPREADSHEETS_READONLY);
+		error_log("Initialized Google API");
+	}
+	
+	public function loadSpreadsheet($sheetid, $rows = 500) : Sheet {
+		$service = new \Google_Service_Sheets($this->client);
+		error_log("Retrieving spreadsheet $sheetid");
+		$sdata = $service->spreadsheets->get($sheetid, [
+			'includeGridData' => true,
+			'ranges' => 'A1:Z'.$rows,
+		]);
+		error_log("Loaded data from: " . $sdata->getSpreadsheetUrl());
+		return new Sheet($sdata);
+	}
+}
+
