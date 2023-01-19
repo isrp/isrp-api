bdf26c120bd398fe5f6319c4ee3874cd1c0951cd
Oded Arbel <oded@geek.co.il>
2023-01-19 14:33:35 +0200

    have the token generator handle knowledge of where the salt is in generated tokens

--- src/Isrp/Api/DragonClub.php
+++ src/Isrp/Api/DragonClub.php
@@ -110,7 +110,7 @@ class DragonClub extends Controller {
 		if (!isset($card['member_number']))
 			return false;
 		$email = trim($record['email']);
-		$salt = $salt ?: bin2hex(random_bytes(2));
+		$salt = $salt ? substr($salt, 0, 4) : bin2hex(random_bytes(2));
 		return $salt . substr(md5($salt . $email.$card['member_number']), 0, 6);
 	}
 	
@@ -123,7 +123,7 @@ class DragonClub extends Controller {
 		$id = trim($id);
 		foreach ($this->dragonMembers() as $record) {
 			$email = trim($record['email']);
-			if ($id == $this->generateMemberToken($record, substr($id, 0, 4)))
+			if ($id == $this->generateMemberToken($record, $id))
 				return true;
 		}
 		return false;
@@ -139,7 +139,7 @@ class DragonClub extends Controller {
 		foreach ($this->dragonMembers() as $record) {
 			if (!@$record['member_number']) continue;
 			$email = trim($record['email'] ?: '');
-			if ($id == $this->generateMemberToken($record, substr($id, 0, 4)))
+			if ($id == $this->generateMemberToken($record, $id))
 				return $record;
 		}
 		return false;
